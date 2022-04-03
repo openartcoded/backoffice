48909d1bcd49e3d7ec680e453fdd5f8622f25046
Nordine Bittich <nordine@bittich.be>
2022-04-03 12:25:10 +0200

    release

--- release.sh
+++ release.sh
@@ -2,11 +2,13 @@
 git checkout main
 git pull
 
-npm version v$1
+npm version $1
 npm i
 git add .
 
-echo "push tag v$1..."
-git push origin v$1
+echo "push tag $1..."
+git push origin $1
+
+git push
 
 echo "done."
