1d8160c17b49765deb6e889672d673e8958665f2
Nordine Bittich <nordine@bittich.be>
2022-04-03 12:23:25 +0200

    release

--- release.sh
+++ release.sh
@@ -9,12 +9,4 @@ git add .
 echo "push tag v$1..."
 git push origin v$1
 
-echo "push docker image v$1..."
-
-docker build -t nbittich/back-office:v$1 .
-docker tag nbittich/back-office:v$1 nbittich/back-office:v$1
-docker push nbittich/back-office:v$1
-
-git push
-
 echo "done."
