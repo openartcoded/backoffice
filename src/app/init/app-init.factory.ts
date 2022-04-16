48e3e115aafbba672f353262429024c1f169a7b9
Nordine Bittich <nordine@bittich.be>
2022-04-16 14:30:01 +0200

    keycloak url (breaking keycloak 17)

--- src/app/init/app-init.factory.ts
+++ src/app/init/app-init.factory.ts
@@ -8,7 +8,7 @@ export function initialize(keycloak: KeycloakService, configService: ConfigInitS
 
     return keycloak.init({
       config: {
-        url: config['KEYCLOAK_URL'] + '/auth',
+        url: config['KEYCLOAK_URL'],
         realm: config['KEYCLOAK_REALM'],
         clientId: config['KEYCLOAK_CLIENT_ID'],
       },
