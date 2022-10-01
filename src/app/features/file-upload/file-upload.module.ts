f72809472f02ac482b1caac9add0f5da07a76e35
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2022-10-01 23:18:53 +0200

    Feat/design improvements (#29)

--- src/app/features/file-upload/file-upload.module.ts
+++ src/app/features/file-upload/file-upload.module.ts
@@ -4,10 +4,9 @@ import { FileUploadTableComponent } from './file-upload-table/file-upload-table.
 import { SharedModule } from '@shared/shared.module';
 import { FileUploadRoutingModule } from './file-upload-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbCollapseModule, NgbDropdownModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbDropdownModule, NgbPaginationModule,  } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FileUploadSearchFormComponent } from './file-upload-search-form/file-upload-search-form.component';
-
 @NgModule({
   declarations: [FileUploadTableComponent, FileUploadSearchFormComponent],
   imports: [
