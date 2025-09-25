d81f0de683a8266303e0c65630639cb1bd9d8735
Nordine Bittich <contact@bittich.be>
2025-09-25 15:42:33 +0200

    add country code

--- src/app/core/models/billable-client.ts
+++ src/app/core/models/billable-client.ts
@@ -2,64 +2,65 @@ import { RateType } from './common';
 import { FileUpload } from './file-upload';
 
 export interface BillableClient {
-  id?: string;
-  maxDaysToPay?: number;
-  rate?: number;
-  taxRate?: number;
-  nature?: string;
-  rateType?: RateType;
-  projectName?: string;
-  vatNumber?: string;
-  address?: string;
-  city?: string;
-  name?: string;
-  emailAddress?: string;
-  phoneNumber?: string;
-  contractStatus?: ContractStatus;
-  startDate?: Date;
-  endDate?: Date;
-  documentIds?: string[];
-  documents?: FileUpload[];
-  imported?: boolean;
-  importedDate?: Date;
-  defaultWorkingDays: DefaultWorkingDay[];
+    id?: string;
+    maxDaysToPay?: number;
+    countryCode?: string;
+    rate?: number;
+    taxRate?: number;
+    nature?: string;
+    rateType?: RateType;
+    projectName?: string;
+    vatNumber?: string;
+    address?: string;
+    city?: string;
+    name?: string;
+    emailAddress?: string;
+    phoneNumber?: string;
+    contractStatus?: ContractStatus;
+    startDate?: Date;
+    endDate?: Date;
+    documentIds?: string[];
+    documents?: FileUpload[];
+    imported?: boolean;
+    importedDate?: Date;
+    defaultWorkingDays: DefaultWorkingDay[];
 }
 
 export function getAllDays(): { name: string; value: string }[] {
-  return [
-    {
-      name: DefaultWorkingDay[DefaultWorkingDay.MONDAY].toLowerCase(),
-      value: DefaultWorkingDay[DefaultWorkingDay.MONDAY],
-    },
-    {
-      name: DefaultWorkingDay[DefaultWorkingDay.TUESDAY].toLowerCase(),
-      value: DefaultWorkingDay[DefaultWorkingDay.TUESDAY],
-    },
-    {
-      name: DefaultWorkingDay[DefaultWorkingDay.WEDNESDAY].toLowerCase(),
-      value: DefaultWorkingDay[DefaultWorkingDay.WEDNESDAY],
-    },
-    {
-      name: DefaultWorkingDay[DefaultWorkingDay.THURSDAY].toLowerCase(),
-      value: DefaultWorkingDay[DefaultWorkingDay.THURSDAY],
-    },
-    {
-      name: DefaultWorkingDay[DefaultWorkingDay.FRIDAY].toLowerCase(),
-      value: DefaultWorkingDay[DefaultWorkingDay.FRIDAY],
-    },
-  ];
+    return [
+        {
+            name: DefaultWorkingDay[DefaultWorkingDay.MONDAY].toLowerCase(),
+            value: DefaultWorkingDay[DefaultWorkingDay.MONDAY],
+        },
+        {
+            name: DefaultWorkingDay[DefaultWorkingDay.TUESDAY].toLowerCase(),
+            value: DefaultWorkingDay[DefaultWorkingDay.TUESDAY],
+        },
+        {
+            name: DefaultWorkingDay[DefaultWorkingDay.WEDNESDAY].toLowerCase(),
+            value: DefaultWorkingDay[DefaultWorkingDay.WEDNESDAY],
+        },
+        {
+            name: DefaultWorkingDay[DefaultWorkingDay.THURSDAY].toLowerCase(),
+            value: DefaultWorkingDay[DefaultWorkingDay.THURSDAY],
+        },
+        {
+            name: DefaultWorkingDay[DefaultWorkingDay.FRIDAY].toLowerCase(),
+            value: DefaultWorkingDay[DefaultWorkingDay.FRIDAY],
+        },
+    ];
 }
 
 export enum DefaultWorkingDay {
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
 }
 
 export enum ContractStatus {
-  NOT_STARTED_YET,
-  ONGOING,
-  DONE,
+    NOT_STARTED_YET,
+    ONGOING,
+    DONE,
 }
--- src/app/core/models/personal.info.ts
+++ src/app/core/models/personal.info.ts
@@ -1,28 +1,29 @@
 export interface PersonalInfo {
-  id?: string;
-  dateCreation?: Date;
-  updatedDate?: Date;
-  organizationName?: string;
-  vatNumber?: string;
-  organizationAddress?: string;
-  organizationPostCode?: string;
-  organizationCity?: string;
-  organizationBankAccount?: string;
-  organizationBankBIC?: string;
-  organizationPhoneNumber?: string;
-  organizationEmailAddress?: string;
-  financeCharge?: number;
-  ceoFullName?: string;
-  note?: string;
-  logoUploadId?: string;
-  signatureUploadId?: string;
-  initialUploadId?: string;
-  maxDaysToPay?: number;
-  accountants?: Accountant[];
+    id?: string;
+    dateCreation?: Date;
+    updatedDate?: Date;
+    countryCode?: string;
+    organizationName?: string;
+    vatNumber?: string;
+    organizationAddress?: string;
+    organizationPostCode?: string;
+    organizationCity?: string;
+    organizationBankAccount?: string;
+    organizationBankBIC?: string;
+    organizationPhoneNumber?: string;
+    organizationEmailAddress?: string;
+    financeCharge?: number;
+    ceoFullName?: string;
+    note?: string;
+    logoUploadId?: string;
+    signatureUploadId?: string;
+    initialUploadId?: string;
+    maxDaysToPay?: number;
+    accountants?: Accountant[];
 }
 export interface Accountant {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber?: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber?: string;
 }
