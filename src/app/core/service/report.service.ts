920d7d4892a0aa5f036d0dd68e7d3c15fa8ad6aa
Nordine Bittich <contact@bittich.be>
2025-11-02 21:30:22 +0100

    formatting report

--- src/app/core/service/report.service.ts
+++ src/app/core/service/report.service.ts
@@ -8,73 +8,73 @@ import { FileService } from '@core/service/file.service';
 import { ConfigInitService } from '@init/config-init.service';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class ReportService {
-    basePath: string;
-    constructor(
-        private http: HttpClient,
-        private configService: ConfigInitService,
-        private fileService: FileService,
-    ) {
-        this.basePath = `${this.configService.getConfig()['BACKEND_URL']}/api/report`;
-    }
+  basePath: string;
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigInitService,
+    private fileService: FileService,
+  ) {
+    this.basePath = `${this.configService.getConfig()['BACKEND_URL']}/api/report`;
+  }
 
-    save(formData: FormData): Observable<Post> {
-        return this.http.post<Post>(`${this.basePath}/submit`, formData, {});
-    }
+  save(formData: FormData): Observable<Post> {
+    return this.http.post<Post>(`${this.basePath}/submit`, formData, {});
+  }
 
-    newPost(): Observable<Post> {
-        return this.http.post<Post>(`${this.basePath}/new-post`, {});
-    }
+  newPost(): Observable<Post> {
+    return this.http.post<Post>(`${this.basePath}/new-post`, {});
+  }
 
-    delete(post: Post): Observable<any> {
-        return this.http.delete<any>(`${this.basePath}?id=${post.id}`);
-    }
+  delete(post: Post): Observable<any> {
+    return this.http.delete<any>(`${this.basePath}?id=${post.id}`);
+  }
 
-    getTags(): Observable<string[]> {
-        return this.http.get<string[]>(`${this.basePath}/tags`);
-    }
+  getTags(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.basePath}/tags`);
+  }
 
-    publicSearch(criteria: PostSearchCriteria, page: number, pageSize: number): Observable<Page<Post>> {
-        return this.http.post<Page<Post>>(
-            `${this.basePath}/public-search?page=${page}&size=${pageSize}&sort=updatedDate,DESC`,
-            criteria,
-        );
-    }
-    addAttachment(postId: string, files: File[]): Observable<Post> {
-        let formData = new FormData();
-        formData.append('id', postId);
-        files.forEach(file => formData.append('files', file));
-        return this.http.post<Post>(`${this.basePath}/add-attachment`, formData);
-    }
-    removeAttachment(postId: string, attachmentId: string): Observable<Post> {
-        return this.http.post<Post>(`${this.basePath}/remove-attachment?id=${postId}&attachmentId=${attachmentId}`, {});
-    }
-    adminSearch(criteria: PostSearchCriteria, page: number, pageSize: number): Observable<Page<Post>> {
-        return this.http.post<Page<Post>>(
-            `${this.basePath}/admin-search?page=${page}&size=${pageSize}&sort=updatedDate,DESC`,
-            criteria,
-        );
-    }
+  publicSearch(criteria: PostSearchCriteria, page: number, pageSize: number): Observable<Page<Post>> {
+    return this.http.post<Page<Post>>(
+      `${this.basePath}/public-search?page=${page}&size=${pageSize}&sort=updatedDate,DESC`,
+      criteria,
+    );
+  }
+  addAttachment(postId: string, files: File[]): Observable<Post> {
+    let formData = new FormData();
+    formData.append('id', postId);
+    files.forEach((file) => formData.append('files', file));
+    return this.http.post<Post>(`${this.basePath}/add-attachment`, formData);
+  }
+  removeAttachment(postId: string, attachmentId: string): Observable<Post> {
+    return this.http.post<Post>(`${this.basePath}/remove-attachment?id=${postId}&attachmentId=${attachmentId}`, {});
+  }
+  adminSearch(criteria: PostSearchCriteria, page: number, pageSize: number): Observable<Page<Post>> {
+    return this.http.post<Page<Post>>(
+      `${this.basePath}/admin-search?page=${page}&size=${pageSize}&sort=updatedDate,DESC`,
+      criteria,
+    );
+  }
 
-    updatePostIts(id: string, postIts: { todos: PostIt[]; inProgress: PostIt[]; done: PostIt[] }): Observable<Post> {
-        return this.http.post<Post>(`${this.basePath}/update-post-it?id=${id}`, postIts);
-    }
+  updatePostIts(id: string, postIts: { todos: PostIt[]; inProgress: PostIt[]; done: PostIt[] }): Observable<Post> {
+    return this.http.post<Post>(`${this.basePath}/update-post-it?id=${id}`, postIts);
+  }
 
-    getPostById(id: string): Observable<Post> {
-        return this.http.post<Post>(`${this.basePath}/post-by-id?id=${id}`, {});
-    }
+  getPostById(id: string): Observable<Post> {
+    return this.http.post<Post>(`${this.basePath}/post-by-id?id=${id}`, {});
+  }
 
-    getPublicPostById(id: string, title: string): Observable<Post> {
-        return this.http.get<Post>(`${this.basePath}/post/${title}/${id}`);
-    }
+  getPublicPostById(id: string, title: string): Observable<Post> {
+    return this.http.get<Post>(`${this.basePath}/post/${title}/${id}`);
+  }
 
-    getLatest(): Observable<Post[]> {
-        return this.http.get<Post[]>(`${this.basePath}/latest`);
-    }
+  getLatest(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.basePath}/latest`);
+  }
 
-    generatePdf(post: Post) {
-        this.fileService.downloadLink(`${this.basePath}/generate-pdf?id=${post.id}`);
-    }
+  generatePdf(post: Post) {
+    this.fileService.downloadLink(`${this.basePath}/generate-pdf?id=${post.id}`);
+  }
 }
--- src/app/shared/image-loader/image-loader.component.ts
+++ src/app/shared/image-loader/image-loader.component.ts
@@ -4,33 +4,33 @@ import { FileUpload } from '@core/models/file-upload';
 import { FileService } from '@core/service/file.service';
 
 @Component({
-    selector: 'app-image-loader',
-    templateUrl: './image-loader.component.html',
-    styleUrls: ['./image-loader.component.scss'],
-    standalone: false,
+  selector: 'app-image-loader',
+  templateUrl: './image-loader.component.html',
+  styleUrls: ['./image-loader.component.scss'],
+  standalone: false,
 })
 export class ImageLoaderComponent implements OnInit {
-    @Input() loader: string = '/assets/img/loader.gif';
-    @Input() image: FileUpload;
-    @Input() cssClass: string;
+  @Input() loader: string = '/assets/img/loader.gif';
+  @Input() image: FileUpload;
+  @Input() cssClass: string;
 
-    imageSrc: any;
-    isLoading: boolean;
+  imageSrc: any;
+  isLoading: boolean;
 
-    constructor(
-        private fileService: FileService,
-        private domSanitizationService: DomSanitizer,
-    ) { }
+  constructor(
+    private fileService: FileService,
+    private domSanitizationService: DomSanitizer,
+  ) {}
 
-    hideLoader() {
-        this.isLoading = false;
-    }
+  hideLoader() {
+    this.isLoading = false;
+  }
 
-    ngOnInit(): void {
-        this.isLoading = true;
-        this.fileService.toDownloadLink(this.fileService.getDownloadUrl(this.image.id)).subscribe((link) => {
-            this.imageSrc = this.domSanitizationService.bypassSecurityTrustUrl(link.href);
-            this.isLoading = false;
-        });
-    }
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.fileService.toDownloadLink(this.fileService.getDownloadUrl(this.image.id)).subscribe((link) => {
+      this.imageSrc = this.domSanitizationService.bypassSecurityTrustUrl(link.href);
+      this.isLoading = false;
+    });
+  }
 }
