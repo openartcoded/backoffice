70d1bc948b40c5e14d32a4d36bdb734a378d6dfd
Nordine Bittich <contact@bittich.be>
2025-11-08 07:21:23 +0100

    fix timesheet trim

--- src/app/shared/pipes/trim.pipe.ts
+++ src/app/shared/pipes/trim.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'trim',
-  standalone: false,
+    name: 'trim',
+    standalone: false,
 })
 export class TrimPipe implements PipeTransform {
-  transform(value: string, length: number): string {
-    const sub = value.substring(0, length);
-    if (sub.length != value.length) {
-      return sub + '...';
+    transform(value: string, length: number, withDots = true): string {
+        const sub = value.substring(0, length);
+        if (sub.length != value.length) {
+            return sub + (withDots ? '...' : '');
+        }
+        return sub;
     }
-    return sub;
-  }
 }
