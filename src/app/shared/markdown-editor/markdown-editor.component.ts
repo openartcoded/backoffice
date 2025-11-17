eed1bf6918ded61808508fcb1d033783b3f3c452
Nordine Bittich <contact@bittich.be>
2025-11-17 15:50:13 +0100

    qol

--- src/app/shared/markdown-editor/markdown-editor.component.ts
+++ src/app/shared/markdown-editor/markdown-editor.component.ts
@@ -1,64 +1,64 @@
 import {
-    Component,
-    AfterViewInit,
-    OnDestroy,
-    ViewChild,
-    ElementRef,
-    Input,
-    Output,
-    EventEmitter,
-    OnInit,
+  Component,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
 } from '@angular/core';
 import EasyMDE from 'easymde';
 
 @Component({
-    selector: 'app-markdown-editor',
-    templateUrl: './markdown-editor.component.html',
-    styleUrls: ['./markdown-editor.component.scss'],
-    standalone: false,
+  selector: 'app-markdown-editor',
+  templateUrl: './markdown-editor.component.html',
+  styleUrls: ['./markdown-editor.component.scss'],
+  standalone: false,
 })
 export class MarkdownEditorComponent implements AfterViewInit, OnInit, OnDestroy {
-    ngOnInit(): void { }
-    @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
-    @Input() initialValue: string = '';
-    @Output() contentChange = new EventEmitter<string>();
+  ngOnInit(): void {}
+  @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
+  @Input() initialValue: string = '';
+  @Output() contentChange = new EventEmitter<string>();
 
-    private easyMDE!: EasyMDE;
+  private easyMDE!: EasyMDE;
 
-    ngAfterViewInit(): void {
-        this.easyMDE = new EasyMDE({
-            element: this.textarea.nativeElement,
-            sideBySideFullscreen: false,
-            initialValue: this.initialValue,
-            nativeSpellcheck: true,
-            autofocus: true,
-            minHeight: '50vh',
-            hideIcons: ['fullscreen'],
-            syncSideBySidePreviewScroll: true,
-            imagesPreviewHandler: (src) => {
-                return `<img src="${src}" alt="Image preview" style="max-width: 100%;">`;
-            },
-            spellChecker: true,
-        });
-        setTimeout(() => {
-            EasyMDE.toggleSideBySide(this.easyMDE);
-            this.easyMDE.codemirror.refresh();
-        }, 200);
-        this.easyMDE.codemirror.on('change', () => {
-            this.contentChange.emit(this.easyMDE.value());
-        });
-    }
+  ngAfterViewInit(): void {
+    this.easyMDE = new EasyMDE({
+      element: this.textarea.nativeElement,
+      sideBySideFullscreen: false,
+      initialValue: this.initialValue,
+      nativeSpellcheck: true,
+      autofocus: true,
+      minHeight: '50vh',
+      hideIcons: ['fullscreen'],
+      syncSideBySidePreviewScroll: true,
+      imagesPreviewHandler: (src) => {
+        return `<img src="${src}" alt="Image preview" style="max-width: 100%;">`;
+      },
+      spellChecker: true,
+    });
+    setTimeout(() => {
+      EasyMDE.toggleSideBySide(this.easyMDE);
+      this.easyMDE.codemirror.refresh();
+    }, 200);
+    this.easyMDE.codemirror.on('change', () => {
+      this.contentChange.emit(this.easyMDE.value());
+    });
+  }
 
-    ngOnDestroy(): void {
-        this.easyMDE.toTextArea();
-        this.easyMDE = null!;
-    }
+  ngOnDestroy(): void {
+    this.easyMDE.toTextArea();
+    this.easyMDE = null!;
+  }
 
-    getValue(): string {
-        return this.easyMDE.value();
-    }
+  getValue(): string {
+    return this.easyMDE.value();
+  }
 
-    setValue(value: string) {
-        this.easyMDE.value(value);
-    }
+  setValue(value: string) {
+    this.easyMDE.value(value);
+  }
 }
